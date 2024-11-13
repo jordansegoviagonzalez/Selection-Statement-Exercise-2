e861e0ef6788e6815fe1e80a6f0fe85f3f0c565b
Jordan Segovia <segoviajordan91@gmail.com>
2024-11-13 00:05:51 -0800

    Asked the user to input their favorite school subject and stored their response in a variable

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            favoriteSubject();
+        }
+        //public stattic is the Access Modifier
+        //void is the return type
+        //favoriteSubjects is the Type name
+
+        public static void favoriteSubject()
+        {
+            Console.WriteLine("What is your favorite subject?!");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+               //case 
+                case "math":
+                    Console.WriteLine("I don't prefer math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "pe":
+                case "p.e":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite too!");
+                    break;
+                case "geography":
+                    Console.WriteLine("I like to study the world too!");
+                    break;
+                default:
+                    Console.WriteLine("I like that too!");
+                    break;
+                
+            }
+            
+            
+            {
+                
+            }
         }
     }
 }
